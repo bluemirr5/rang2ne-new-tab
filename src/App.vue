f64c4c5a0dad0f387bff680a7676c29eda5813f7
gswon <bluemirr5@gmail.com>
2017-11-18 02:50:40 +0900

    add clock component

--- src/App.vue
+++ src/App.vue
@@ -26,10 +26,9 @@ export default {
     overflow: hidden;
     font-family: 'Roboto', 'Arial', 'Malgun Gothic', '맑은 고딕', sans-serif;
     --background: #f0efee;
-    background-position: center;
     /* min-height: 635px; */
     user-select: none;
-    background-image: url(https://whale-store.pstatic.net/20170919_104/15058224522968TW0x_JPEG/irene-coco-153263.jpg), url(https://whale-store.pstatic.net/20170919_104/15058224522968TW0x_JPEG/irene-coco-153263.jpg?type=a320);
+    background: url(https://whale-store.pstatic.net/20170919_104/15058224522968TW0x_JPEG/irene-coco-153263.jpg), url(https://whale-store.pstatic.net/20170919_104/15058224522968TW0x_JPEG/irene-coco-153263.jpg?type=a320) center;
   }
 
   body {
